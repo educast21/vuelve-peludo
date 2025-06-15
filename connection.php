97005751e623142d7c321e770531b34b064d0794
Alejandro <alexabundes2942@gmail.com>
2025-06-14 23:42:04 -0600

    Archivos base

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+        $dbHost = 'localhost';
+        $dbUser = 'root';
+        $dbPass = '';
+        $dbName = 'vuelvepeludo';
+        $connection = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+?>
